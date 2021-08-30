f621dfa7447cd877d855e93f5436c47e766d6214
shreysh-mane <shreysh.mane12@gmail.com>
2021-08-30 14:03:44 +0530

    debug- form

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,21 +1,24 @@
 const quiz = document.querySelector(".quiz");
-const submitBtn = document.querySelector("#submint-btn");
+const submitBtn = document.querySelector("#submit-btn");
 const showAns = document.querySelector("#show-ans");
 
-const ans = ["PR² + QR² = PQ²", "4.5 cm"];
+const ans = ["PR² + QR² = PQ²", "4.5 cm", "Two sides are equal", "Equilateral"];
 
-function checkAns() {
+function checkAns(e) {
+  e.preventDefault();
   let score = 0;
   let index = 0;
-  showAns.textContent = `your sco`;
   const formResults = new FormData(quiz);
+  // console.log(formResults);
+
   for (let uans of formResults.values()) {
     if (uans == ans[index]) {
       score += 1;
     }
+    index += 1;
   }
-  index += 1;
-  //   showAns.textContent = `your score is ${score}`;
+
+  showAns.textContent = `your score is ${score}`;
 }
 
 submitBtn.addEventListener("click", checkAns);
